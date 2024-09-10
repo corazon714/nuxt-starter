de6f78bc369f0379f3034b008202fd3bdfeafd94
corazon <mert.kaya.98@protonmail.com>
2024-09-10 03:43:17 +0300

    chore: Add dynamic page title based on route path and set page-layout transitions

--- app.vue
+++ app.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const pageTitle = computed(() => {
+  const pathSegments = route.path.split('/');
+  const lastSegment = pathSegments[pathSegments.length - 1];
+  const title = lastSegment ? `${lastSegment.charAt(0).toUpperCase() + lastSegment.slice(1)} | {Site Name}` : '{Site Name}';
+  return title;
+});
+
+useHead({
+  title: pageTitle
+})
+</script>
+
 <template>
   <div class="font-roboto">
     <NuxtLayout>
@@ -21,5 +36,21 @@ Body {
 
 .font-roboto {
   font-family: 'Roboto', sans-serif;
+}
+
+
+.layout-enter-active,
+.layout-leave-active,
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.2s;
+}
+
+.layout-enter-from,
+.layout-leave-to,
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
 }
 </style>
